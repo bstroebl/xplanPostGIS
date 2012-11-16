33feb5ea06091436ee214448108809f1996dcfb3
Bernhard Ströbl <bernhard.stroebl@jena.de>
2012-11-16 07:48:03 +0100

    Ermögliche Sperre von Bereichen

--- QGIS.sql
+++ QGIS.sql
@@ -3,6 +3,40 @@ COMMENT ON SCHEMA "QGIS" IS 'Hilfstabellen zur Nutzung von XPlan in QGIS';
 
 GRANT USAGE ON SCHEMA "QGIS" TO xp_gast;
 
+-- *****************************************************
+-- CREATE TRIGGER FUNCTIONs
+-- *****************************************************
+
+CREATE OR REPLACE FUNCTION "QGIS"."XP_Bereich_Sperre"() 
+RETURNS trigger AS
+$BODY$ 
+ BEGIN
+    INSERT INTO "QGIS"."XP_Bereich_gesperrt"("XP_Bereich_gid") VALUES (new.gid);
+ END; $BODY$
+  LANGUAGE 'plpgsql' VOLATILE
+  COST 100;
+GRANT EXECUTE ON FUNCTION "QGIS"."XP_Bereich_Sperre"() TO xp_user;
+
+
+-- *****************************************************
+-- CREATE TABLEs
+-- *****************************************************
+-- -----------------------------------------------------
+-- Table "QGIS"."XP_Bereich_gesperrt"
+-- -----------------------------------------------------
+CREATE  TABLE  "QGIS"."XP_Bereich_gesperrt" (
+  "XP_Bereich_gid" INTEGER NOT NULL ,
+  "gesperrt" BOOLEAN  NOT NULL DEFAULT f ,
+  PRIMARY KEY ("XP_Bereich_gid") ,
+  CONSTRAINT "fk_XP_Bereich_gesperrt_XP_Bereich1"
+    FOREIGN KEY ("XP_Bereich_gid" )
+    REFERENCES "XP_Basisobjekte"."XP_Bereich" ("gid" )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+CREATE INDEX "idx_fk_XP_Bereich_gesperrt_XP_Bereich1" ON "QGIS"."XP_Bereich_gesperrt" ("XP_Bereich_gid") ;
+GRANT SELECT ON TABLE "QGIS"."XP_Bereich_gesperrt" TO xp_gast;
+GRANT ALL ON TABLE "QGIS"."XP_Bereich_gesperrt" TO xp_user;
+
 -- -----------------------------------------------------
 -- Table "QGIS"."HorizontaleAusrichtung"
 -- -----------------------------------------------------
@@ -34,6 +68,12 @@ CREATE  TABLE  "QGIS"."VertikaleAusrichtung" (
 
 CREATE INDEX "idx_fk_VertikaleAusrichtung_XP_VertikaleAusrichtung1" ON "QGIS"."VertikaleAusrichtung" ("Wert") ;
 GRANT SELECT ON TABLE "QGIS"."VertikaleAusrichtung" TO xp_gast;
+
+-- *****************************************************
+-- Amend Tabless
+-- *****************************************************
+CREATE TRIGGER "XP_Bereich_hasInsert" AFTER INSERT ON "XP_Basisobjekte"."XP_Bereich" FOR EACH ROW EXECUTE PROCEDURE "QGIS"."XP_Bereich_Sperre"();
+
 
 -- -----------------------------------------------------
 -- Data for table "QGIS"."HorizontaleAusrichtung"
