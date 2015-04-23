451116612e2107b638d8af61ada5abcebda2c336
Bernhard Ströbl <bernhard.stroebl@jena.de>
2015-04-23 15:59:12 +0200

    Stelle sicher, dass auch Überlagerungsobjekte RHR folgen

--- XP_Basisschema.sql
+++ XP_Basisschema.sql
@@ -215,21 +215,6 @@ $BODY$
   COST 100;
 GRANT EXECUTE ON FUNCTION "XP_Basisobjekte"."isAbstract"() TO xp_user;
 
-CREATE OR REPLACE FUNCTION "XP_Basisobjekte"."isUeberlagerungsobjekt"() 
-RETURNS trigger AS
-$BODY$ 
- BEGIN
-    IF (TG_OP = 'DELETE') THEN
-        RETURN old;
-    ELSE
-        new.flaechenschluss := false;
-        RETURN new;
-    END IF;
- END; $BODY$
-  LANGUAGE 'plpgsql' VOLATILE
-  COST 100;
-GRANT EXECUTE ON FUNCTION "XP_Basisobjekte"."isUeberlagerungsobjekt"() TO xp_user;
-
 CREATE OR REPLACE FUNCTION "XP_Basisobjekte"."change_to_XP_Plan"() 
 RETURNS trigger AS
 $BODY$ 
@@ -563,6 +548,25 @@ $BODY$
   LANGUAGE 'plpgsql' VOLATILE
   COST 100;
 GRANT EXECUTE ON FUNCTION "XP_Basisobjekte"."isFlaechenschlussobjekt"() TO xp_user;
+
+CREATE OR REPLACE FUNCTION "XP_Basisobjekte"."isUeberlagerungsobjekt"() 
+RETURNS trigger AS
+$BODY$ 
+ BEGIN
+	IF new."position" IS NOT NULL THEN
+        new."position" := ST_ForceRHR(new."position");
+    END IF;
+    
+    IF (TG_OP = 'DELETE') THEN
+        RETURN old;
+    ELSE
+        new.flaechenschluss := false;
+        RETURN new;
+    END IF;
+ END; $BODY$
+  LANGUAGE 'plpgsql' VOLATILE
+  COST 100;
+GRANT EXECUTE ON FUNCTION "XP_Basisobjekte"."isUeberlagerungsobjekt"() TO xp_user;
 
 CREATE OR REPLACE FUNCTION "XP_Basisobjekte"."positionFollowsRHR"() 
 RETURNS trigger AS
