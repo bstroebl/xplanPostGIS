72a91e134e46477881d56eeccd56d0a5bbcafd96
Bernhard Ströbl <bernhard.stroebl@jena.de>
2015-07-15 14:24:23 +0200

    [BUG] Passe View an neue Feldnamen an

--- FP_Fachschema_FPlan.sql
+++ FP_Fachschema_FPlan.sql
@@ -2822,20 +2822,27 @@ CREATE OR REPLACE RULE _delete AS
 -- View "FP_Basisobjekte"."FP_Objekte"
 -- -----------------------------------------------------
 CREATE OR REPLACE VIEW "FP_Basisobjekte"."FP_Objekte" AS
- SELECT fp_o.gid, g."FP_Bereich_gid", fp_o."Objektart", fp_o."Objektartengruppe"
- FROM
-  ( SELECT o.gid, c.relname::character varying AS "Objektart", n.nspname::character varying AS "Objektartengruppe"
-         FROM (        (         SELECT p.gid, p.tableoid
-                                 FROM "FP_Basisobjekte"."FP_Punktobjekt" p
-                      UNION
-                               SELECT "FP_Linienobjekt".gid, "FP_Linienobjekt".tableoid
-                                 FROM "FP_Basisobjekte"."FP_Linienobjekt")
-              UNION
-                       SELECT "FP_Flaechenobjekt".gid, "FP_Flaechenobjekt".tableoid
-                         FROM "FP_Basisobjekte"."FP_Flaechenobjekt") o
-    JOIN pg_class c ON o.tableoid = c.oid
-    JOIN pg_namespace n ON c.relnamespace = n.oid) fp_o
-    left join "FP_Basisobjekte"."FP_Objekt_gehoertZuFP_Bereich" g ON fp_o.gid = g."FP_Objekt_gid";
+ SELECT fp_o.gid,
+    g."gehoertZuFP_Bereich" AS "FP_Bereich_gid",
+    fp_o."Objektart",
+    fp_o."Objektartengruppe"
+   FROM ( SELECT o.gid,
+            c.relname::character varying AS "Objektart",
+            n.nspname::character varying AS "Objektartengruppe"
+           FROM ( SELECT p.gid,
+                    p.tableoid
+                   FROM "FP_Basisobjekte"."FP_Punktobjekt" p
+                UNION
+                 SELECT "FP_Linienobjekt".gid,
+                    "FP_Linienobjekt".tableoid
+                   FROM "FP_Basisobjekte"."FP_Linienobjekt"
+                UNION
+                 SELECT "FP_Flaechenobjekt".gid,
+                    "FP_Flaechenobjekt".tableoid
+                   FROM "FP_Basisobjekte"."FP_Flaechenobjekt") o
+             JOIN pg_class c ON o.tableoid = c.oid
+             JOIN pg_namespace n ON c.relnamespace = n.oid) fp_o
+     LEFT JOIN "FP_Basisobjekte"."FP_Objekt_gehoertZuFP_Bereich" g ON fp_o.gid = g."FP_Objekt_gid";
 GRANT SELECT ON TABLE "FP_Basisobjekte"."FP_Objekte" TO xp_gast;
 
 -- *****************************************************
