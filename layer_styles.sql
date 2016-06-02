7e067fa809417f1ae47244fffa9e4255145af498
Bernhard Ströbl <bernhard.stroebl@jena.de>
2016-06-02 09:41:29 +0200

    Lizenziere layer_styles mit GPL

--- layer_styles.sql
+++ layer_styles.sql
@@ -1,6 +1,11 @@
---
--- PostgreSQL database dump
---
+/* *************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ************************************************************************* */
 
 INSERT INTO "QGIS".layer VALUES (25, 'FP_Sonstiges', 'FP_KennzeichnungFlaeche', '<qgis version="2.8.1-Wien" minimumScale="0.0" maximumScale="100000000.0" simplifyDrawingHints="1" minLabelScale="0" maxLabelScale="1e+08" simplifyDrawingTol="1" simplifyMaxScale="1" hasScaleBasedVisibilityFlag="0" simplifyLocal="1" scaleBasedLabelVisibilityFlag="0">
  <edittypes>
@@ -61590,9 +61595,4 @@ def my_form_open(dialog, layer, feature):
 ', NULL, NULL, '#ffec99');
 
 
--- Completed on 2016-03-10 14:54:49
-
---
--- PostgreSQL database dump complete
---
 
