39f421180a5ba6ea43076b9fec76ae007f65f540
Bernhard Ströbl <bernhard.stroebl@jena.de>
2017-09-11 10:52:34 +0200

    [BUG] Korrigiere Feldnamen

--- FP_QGIS.sql
+++ FP_QGIS.sql
@@ -20,23 +20,23 @@ SELECT g.gid,z1 as zweckbestimmung1,z2 as zweckbestimmung2,z3 as zweckbestimmung
  bz1 as "besondereZweckbestimmung1", bz2 as "besondereZweckbestimmung2", bz3 as "besondereZweckbestimmung3", bz4 as "besondereZweckbestimmung4",
  coalesce(bz1 / bz1, 0) + coalesce(bz2 / bz2, 0) + coalesce(bz3 / bz3, 0) + coalesce(bz4 / bz4, 0) as "anz_besondereZweckbestimmung",
  CASE WHEN bz1 IS NULL THEN
-	CASE WHEN 2400 IN(z1,z2,z3,z4) THEN 'SicherheitOrdnung'
-	WHEN 2600 IN (z1,z2,z3,z4) THEN 'Infrastruktur'
-	END
+    CASE WHEN 2400 IN(z1,z2,z3,z4) THEN 'SicherheitOrdnung'
+    WHEN 2600 IN (z1,z2,z3,z4) THEN 'Infrastruktur'
+    END
  ELSE
-	bzl1."Bezeichner" 
+    bzl1."Bezeichner"
 END as label1,
 bzl2."Bezeichner" as label2,
 bzl3."Bezeichner" as label3,
 bzl4."Bezeichner" as label4
-  FROM 
+  FROM
  "FP_Gemeinbedarf_Spiel_und_Sportanlagen"."FP_Gemeinbedarf" g
  LEFT JOIN
- crosstab('SELECT "FP_Gemeinbedarf_gid", "FP_Gemeinbedarf_gid", zweckbestimmung FROM "FP_Gemeinbedarf_Spiel_und_Sportanlagen"."FP_Gemeinbedarf_zweckbestimmung" ORDER BY 1,3') zt 
+ crosstab('SELECT "FP_Gemeinbedarf_gid", "FP_Gemeinbedarf_gid", zweckbestimmung FROM "FP_Gemeinbedarf_Spiel_und_Sportanlagen"."FP_Gemeinbedarf_zweckbestimmung" ORDER BY 1,3') zt
  (zgid bigint, z1 integer,z2 integer,z3 integer,z4 integer)
  ON g.gid=zt.zgid
  LEFT JOIN
- crosstab('SELECT "FP_Gemeinbedarf_gid", "FP_Gemeinbedarf_gid", "besondereZweckbestimmung" FROM "FP_Gemeinbedarf_Spiel_und_Sportanlagen"."FP_Gemeinbedarf_besondereZweckbestimmung" ORDER BY 1,3') bzt 
+ crosstab('SELECT "FP_Gemeinbedarf_gid", "FP_Gemeinbedarf_gid", "besondereZweckbestimmung" FROM "FP_Gemeinbedarf_Spiel_und_Sportanlagen"."FP_Gemeinbedarf_besondereZweckbestimmung" ORDER BY 1,3') bzt
  (bzgid bigint,bz1 integer,bz2 integer,bz3 integer,bz4 integer)
  ON g.gid = bzt.bzgid
   LEFT JOIN "XP_Enumerationen"."XP_BesondereZweckbestGemeinbedarf" bzl1 ON bzt.bz1 = bzl1."Code"
@@ -71,12 +71,12 @@ SELECT g.gid, z1 as zweckbestimmung1,z2 as zweckbestimmung2,z3 as zweckbestimmun
  coalesce(z1 / z1, 0) + coalesce(z2 / z2, 0) + coalesce(z3 / z3, 0) + coalesce(z4 / z4, 0) as anz_zweckbestimmung,
 CASE WHEN 3000 IN(z1,z2,z3,z4) THEN 'SpielSportanlage'
 ELSE
-	''
+    ''
 END as label1
-  FROM 
+  FROM
  "FP_Gemeinbedarf_Spiel_und_Sportanlagen"."FP_SpielSportanlage" g
  LEFT JOIN
- crosstab('SELECT "FP_SpielSportanlage_gid", "FP_SpielSportanlage_gid", zweckbestimmung FROM "FP_Gemeinbedarf_Spiel_und_Sportanlagen"."FP_Gemeinbedarf_zweckbestimmung" ORDER BY 1,3') zt 
+ crosstab('SELECT "FP_SpielSportanlage_gid", "FP_SpielSportanlage_gid", zweckbestimmung FROM "FP_Gemeinbedarf_Spiel_und_Sportanlagen"."FP_Gemeinbedarf_zweckbestimmung" ORDER BY 1,3') zt
  (zgid bigint, z1 integer,z2 integer,z3 integer,z4 integer)
  ON g.gid=zt.zgid;
 GRANT SELECT ON TABLE "FP_Gemeinbedarf_Spiel_und_Sportanlagen"."FP_SpielSportanlage_qv" TO xp_gast;
@@ -108,8 +108,8 @@ SELECT g.gid,z1 as zweckbestimmung1,z2 as zweckbestimmung2,z3 as zweckbestimmung
  bz1 as "besondereZweckbestimmung1", bz2 as "besondereZweckbestimmung2", bz3 as "besondereZweckbestimmung3", bz4 as "besondereZweckbestimmung4",
  coalesce(bz1 / bz1, 0) + coalesce(bz2 / bz2, 0) + coalesce(bz3 / bz3, 0) + coalesce(bz4 / bz4, 0) as "anz_besondereZweckbestimmung",
  CASE WHEN bz1 IS NULL THEN
-	CASE WHEN 2200 IN(z1,z2,z3,z4) THEN 'FreizeitErholung'
-	END
+    CASE WHEN 2200 IN(z1,z2,z3,z4) THEN 'FreizeitErholung'
+    END
 ELSE
     CASE WHEN bz1 IN (12000,14004,16000,22000,24000,24001) THEN
         NULL
@@ -118,17 +118,17 @@ ELSE
     END
 END as label1,
 CASE WHEN bz1 IS NULL THEN
-	CASE WHEN 2400 IN (z1,z2,z3,z4) THEN 'Spez. Gruenflaeche'
-	END
+    CASE WHEN 2400 IN (z1,z2,z3,z4) THEN 'Spez. Gruenflaeche'
+    END
 ELSE
-	CASE WHEN bz2 IN (12000,14004,16000,22000,24000,24001) THEN
-		NULL
-	ELSE
-		bzl2."Bezeichner"
-	END
+    CASE WHEN bz2 IN (12000,14004,16000,22000,24000,24001) THEN
+        NULL
+    ELSE
+        bzl2."Bezeichner"
+    END
 END as label2,
 CASE WHEN bz3 IN (12000,14004,16000,22000,24000,24001) THEN
-	NULL
+    NULL
 ELSE
     bzl3."Bezeichner"
 END as label3,
@@ -138,10 +138,10 @@ ELSE
     bzl4."Bezeichner"
 END as label4
 FROM "FP_Landwirtschaft_Wald_und_Gruen"."FP_Gruen" g
-    LEFT JOIN crosstab('SELECT "FP_Gruen_gid", "FP_Gruen_gid", zweckbestimmung FROM "FP_Landwirtschaft_Wald_und_Gruen"."FP_Gruen_zweckbestimmung" ORDER BY 1,3') zt 
+    LEFT JOIN crosstab('SELECT "FP_Gruen_gid", "FP_Gruen_gid", zweckbestimmung FROM "FP_Landwirtschaft_Wald_und_Gruen"."FP_Gruen_zweckbestimmung" ORDER BY 1,3') zt
         (zgid bigint, z1 integer,z2 integer,z3 integer,z4 integer)
     ON g.gid=zt.zgid
-    LEFT JOIN crosstab('SELECT "FP_Gruen_gid", "FP_Gruen_gid", "besondereZweckbestimmung" FROM "FP_Landwirtschaft_Wald_und_Gruen"."FP_Gruen_besondereZweckbestimmung" ORDER BY 1,3') bzt 
+    LEFT JOIN crosstab('SELECT "FP_Gruen_gid", "FP_Gruen_gid", "besondereZweckbestimmung" FROM "FP_Landwirtschaft_Wald_und_Gruen"."FP_Gruen_besondereZweckbestimmung" ORDER BY 1,3') bzt
         (bzgid bigint,bz1 integer,bz2 integer,bz3 integer,bz4 integer)
     ON g.gid = bzt.bzgid
     LEFT JOIN "XP_Enumerationen"."XP_BesondereZweckbestimmungGruen" bzl1 ON bzt.bz1 = bzl1."Code"
@@ -174,10 +174,10 @@ GRANT SELECT ON TABLE "FP_Landwirtschaft_Wald_und_Gruen"."FP_GruenPunkt_qv" TO x
 CREATE OR REPLACE VIEW "FP_Landwirtschaft_Wald_und_Gruen"."FP_WaldFlaeche_qv" AS
 SELECT g.gid, g.position, z1 as zweckbestimmung1,z2 as zweckbestimmung2,z3 as zweckbestimmung3,z4 as zweckbestimmung4,
  coalesce(z1 / z1, 0) + coalesce(z2 / z2, 0) + coalesce(z3 / z3, 0) + coalesce(z4 / z4, 0) as anz_zweckbestimmung
-  FROM 
+  FROM
  "FP_Landwirtschaft_Wald_und_Gruen"."FP_WaldFlaeche" g
  LEFT JOIN
- crosstab('SELECT "FP_SpielSportanlage_gid", "FP_SpielSportanlage_gid", zweckbestimmung FROM "FP_Landwirtschaft_Wald_und_Gruen"."FP_WaldFlaeche_zweckbestimmung" ORDER BY 1,3') zt 
+ crosstab('SELECT "FP_WaldFlaeche_gid", "FP_WaldFlaeche_gid", zweckbestimmung FROM "FP_Landwirtschaft_Wald_und_Gruen"."FP_WaldFlaeche_zweckbestimmung" ORDER BY 1,3') zt
  (zgid bigint, z1 integer,z2 integer,z3 integer,z4 integer)
  ON g.gid=zt.zgid;
 GRANT SELECT ON TABLE "FP_Landwirtschaft_Wald_und_Gruen"."FP_WaldFlaeche_qv" TO xp_gast;
@@ -189,18 +189,18 @@ CREATE OR REPLACE VIEW "FP_Sonstiges"."FP_Kennzeichnung_qv" AS
 SELECT g.gid, z1 as zweckbestimmung1,z2 as zweckbestimmung2,z3 as zweckbestimmung3,z4 as zweckbestimmung4,
  coalesce(z1 / z1, 0) + coalesce(z2 / z2, 0) + coalesce(z3 / z3, 0) + coalesce(z4 / z4, 0) as anz_zweckbestimmung,
 CASE WHEN z1 != 4000 THEN zl1."Bezeichner"
-END	as label1,
+END as label1,
 CASE WHEN z2 != 4000 THEN zl2."Bezeichner"
-END	as label2,
+END as label2,
 CASE WHEN z3 != 4000 THEN zl3."Bezeichner"
-END	as label3,
+END as label3,
 CASE WHEN z4 != 4000 THEN zl4."Bezeichner"
-END	as label4
-  FROM 
+END as label4
+  FROM
  "FP_Sonstiges"."FP_Kennzeichnung" g
  LEFT JOIN
- crosstab('SELECT "FP_Kennzeichnung_gid", "FP_Kennzeichnung_gid", zweckbestimmung FROM "FP_Sonstiges"."FP_Kennzeichnung_zweckbestimmung" ORDER BY 1,3') zt 
- (zgid bigint, z1 integer,z2 integer,z3 integer,z4 integer) 
+ crosstab('SELECT "FP_Kennzeichnung_gid", "FP_Kennzeichnung_gid", zweckbestimmung FROM "FP_Sonstiges"."FP_Kennzeichnung_zweckbestimmung" ORDER BY 1,3') zt
+ (zgid bigint, z1 integer,z2 integer,z3 integer,z4 integer)
   ON g.gid=zt.zgid
     LEFT JOIN "XP_Enumerationen"."XP_ZweckbestimmungKennzeichnung" zl1 ON zt.z1 = zl1."Code"
     LEFT JOIN "XP_Enumerationen"."XP_ZweckbestimmungKennzeichnung" zl2 ON zt.z2 = zl2."Code"
@@ -238,7 +238,7 @@ GRANT SELECT ON TABLE "FP_Sonstiges"."FP_KennzeichnungPunkt_qv" TO xp_gast;
 -- -----------------------------------------------------
 -- View "FP_Sonstiges"."FP_PrivilegiertesVorhaben_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Sonstiges"."FP_PrivilegiertesVorhaben_qv" AS 
+CREATE OR REPLACE VIEW "FP_Sonstiges"."FP_PrivilegiertesVorhaben_qv" AS
  SELECT g.gid, z1 as zweckbestimmung1,z2 as zweckbestimmung2,z3 as zweckbestimmung3,z4 as zweckbestimmung4,
  coalesce(z1 / z1, 0) + coalesce(z2 / z2, 0) + coalesce(z3 / z3, 0) + coalesce(z4 / z4, 0) as anz_zweckbestimmung,
  bz1 as "besondereZweckbestimmung1", bz2 as "besondereZweckbestimmung2", bz3 as "besondereZweckbestimmung3", bz4 as "besondereZweckbestimmung4",
@@ -263,14 +263,14 @@ CASE WHEN bz1 IS NULL THEN
 ELSE
     bzl4."Bezeichner"
 END as label4
-  FROM 
+  FROM
  "FP_Sonstiges"."FP_PrivilegiertesVorhaben" g
  LEFT JOIN
- crosstab('SELECT "FP_PrivilegiertesVorhaben_gid", "FP_PrivilegiertesVorhaben_gid", zweckbestimmung FROM "FP_Sonstiges"."FP_PrivilegiertesVorhaben_zweckbestimmung" ORDER BY 1,3') zt 
+ crosstab('SELECT "FP_PrivilegiertesVorhaben_gid", "FP_PrivilegiertesVorhaben_gid", zweckbestimmung FROM "FP_Sonstiges"."FP_PrivilegiertesVorhaben_zweckbestimmung" ORDER BY 1,3') zt
  (zgid bigint, z1 integer,z2 integer,z3 integer,z4 integer)
  ON g.gid=zt.zgid
  LEFT JOIN
- crosstab('SELECT "FP_PrivilegiertesVorhaben_gid", "FP_PrivilegiertesVorhaben_gid", "besondereZweckbestimmung" FROM "FP_Sonstiges"."FP_PrivilegiertesVorhaben_besondereZweckbestimmung" ORDER BY 1,3') bzt 
+ crosstab('SELECT "FP_PrivilegiertesVorhaben_gid", "FP_PrivilegiertesVorhaben_gid", "besondereZweckbestimmung" FROM "FP_Sonstiges"."FP_PrivilegiertesVorhaben_besondereZweckbestimmung" ORDER BY 1,3') bzt
  (bzgid bigint,bz1 integer,bz2 integer,bz3 integer,bz4 integer)
  ON g.gid = bzt.bzgid
   LEFT JOIN "FP_Sonstiges"."FP_BesondZweckbestPrivilegiertesVorhaben" bzl1 ON bzt.bz1 = bzl1."Code"
@@ -281,7 +281,7 @@ END as label4
   LEFT JOIN "FP_Sonstiges"."FP_ZweckbestimmungPrivilegiertesVorhaben" zl2 ON zt.z2 = zl2."Code"
   LEFT JOIN "FP_Sonstiges"."FP_ZweckbestimmungPrivilegiertesVorhaben" zl3 ON zt.z3 = zl3."Code"
   LEFT JOIN "FP_Sonstiges"."FP_ZweckbestimmungPrivilegiertesVorhaben" zl4 ON zt.z4 = zl4."Code";
-  
+
 GRANT SELECT ON TABLE "FP_Sonstiges"."FP_PrivilegiertesVorhaben_qv" TO xp_gast;
 
 -- -----------------------------------------------------
@@ -314,30 +314,30 @@ GRANT SELECT ON TABLE "FP_Sonstiges"."FP_PrivilegiertesVorhabenPunkt_qv" TO xp_g
 -- -----------------------------------------------------
 -- View "FP_Ver_und_Entsorgung"."FP_VerEntsorgung_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Ver_und_Entsorgung"."FP_VerEntsorgung_qv" AS 
+CREATE OR REPLACE VIEW "FP_Ver_und_Entsorgung"."FP_VerEntsorgung_qv" AS
  SELECT g.gid, xpo.ebene, z1 as zweckbestimmung1,z2 as zweckbestimmung2,z3 as zweckbestimmung3,z4 as zweckbestimmung4,
  coalesce(z1 / z1, 0) + coalesce(z2 / z2, 0) + coalesce(z3 / z3, 0) + coalesce(z4 / z4, 0) as anz_zweckbestimmung,
  bz1 as "besondereZweckbestimmung1", bz2 as "besondereZweckbestimmung2", bz3 as "besondereZweckbestimmung3", bz4 as "besondereZweckbestimmung4",
  coalesce(bz1 / bz1, 0) + coalesce(bz2 / bz2, 0) + coalesce(bz3 / bz3, 0) + coalesce(bz4 / bz4, 0) as "anz_besondereZweckbestimmung",
  CASE WHEN bz1 IS NULL THEN
-	CASE WHEN 2000 IN(z1,z2,z3,z4) THEN 'Regenwasser'
-	WHEN 2600 IN (z1,z2,z3,z4) THEN 'Telekom.'
-	END
+    CASE WHEN 2000 IN(z1,z2,z3,z4) THEN 'Regenwasser'
+    WHEN 2600 IN (z1,z2,z3,z4) THEN 'Telekom.'
+    END
  ELSE
-	bzl1."Bezeichner" 
+    bzl1."Bezeichner"
 END as label1,
 bzl2."Bezeichner" as label2,
 bzl3."Bezeichner" as label3,
 bzl4."Bezeichner" as label4
-  FROM 
+  FROM
  "FP_Ver_und_Entsorgung"."FP_VerEntsorgung" g
  JOIN "XP_Basisobjekte"."XP_Objekt" xpo ON g.gid = xpo.gid
  LEFT JOIN
- crosstab('SELECT "FP_VerEntsorgung_gid", "FP_VerEntsorgung_gid", zweckbestimmung FROM "FP_Ver_und_Entsorgung"."FP_VerEntsorgung_zweckbestimmung" ORDER BY 1,3') zt 
+ crosstab('SELECT "FP_VerEntsorgung_gid", "FP_VerEntsorgung_gid", zweckbestimmung FROM "FP_Ver_und_Entsorgung"."FP_VerEntsorgung_zweckbestimmung" ORDER BY 1,3') zt
  (zgid bigint, z1 integer,z2 integer,z3 integer,z4 integer)
  ON g.gid=zt.zgid
  LEFT JOIN
- crosstab('SELECT "FP_VerEntsorgung_gid", "FP_VerEntsorgung_gid", "besondereZweckbestimmung" FROM "FP_Ver_und_Entsorgung"."FP_VerEntsorgung_besondereZweckbestimmung" ORDER BY 1,3') bzt 
+ crosstab('SELECT "FP_VerEntsorgung_gid", "FP_VerEntsorgung_gid", "besondereZweckbestimmung" FROM "FP_Ver_und_Entsorgung"."FP_VerEntsorgung_besondereZweckbestimmung" ORDER BY 1,3') bzt
  (bzgid bigint,bz1 integer,bz2 integer,bz3 integer,bz4 integer)
  ON g.gid = bzt.bzgid
   LEFT JOIN "XP_Enumerationen"."XP_BesondereZweckbestimmungVerEntsorgung" bzl1 ON bzt.bz1 = bzl1."Code"
@@ -376,7 +376,7 @@ GRANT SELECT ON TABLE "FP_Ver_und_Entsorgung"."FP_VerEntsorgungPunkt_qv" TO xp_g
 -- -----------------------------------------------------
 -- View "FP_Verkehr"."FP_StrassenverkehrFlaeche_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Verkehr"."FP_StrassenverkehrFlaeche_qv" AS 
+CREATE OR REPLACE VIEW "FP_Verkehr"."FP_StrassenverkehrFlaeche_qv" AS
  SELECT g.gid, g.position, xpo.ebene, xpo.rechtsstand, p.zweckbestimmung as zweckbestimmung1
 FROM "FP_Verkehr"."FP_StrassenverkehrFlaeche" g
     JOIN "FP_Verkehr"."FP_Strassenverkehr" p ON g.gid = p.gid
@@ -386,7 +386,7 @@ GRANT SELECT ON TABLE "FP_Verkehr"."FP_StrassenverkehrFlaeche_qv" TO xp_gast;
 -- -----------------------------------------------------
 -- View "FP_Verkehr"."FP_StrassenverkehrLinie_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Verkehr"."FP_StrassenverkehrLinie_qv" AS 
+CREATE OR REPLACE VIEW "FP_Verkehr"."FP_StrassenverkehrLinie_qv" AS
  SELECT g.gid, g.position, xpo.ebene, xpo.rechtsstand, p.zweckbestimmung as zweckbestimmung1
 FROM "FP_Verkehr"."FP_StrassenverkehrLinie" g
     JOIN "FP_Verkehr"."FP_Strassenverkehr" p ON g.gid = p.gid
@@ -396,8 +396,8 @@ GRANT SELECT ON TABLE "FP_Verkehr"."FP_StrassenverkehrLinie_qv" TO xp_gast;
 -- -----------------------------------------------------
 -- View "FP_Verkehr"."FP_StrassenverkehrPunkt_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Verkehr"."FP_StrassenverkehrPunkt_qv" AS 
- SELECT g.gid, g.position, xpo.ebene, xpo.rechtsstand, p.zweckbestimmung as zweckbestimmung1, 
+CREATE OR REPLACE VIEW "FP_Verkehr"."FP_StrassenverkehrPunkt_qv" AS
+ SELECT g.gid, g.position, xpo.ebene, xpo.rechtsstand, p.zweckbestimmung as zweckbestimmung1,
  p."besondereZweckbestimmung" as "besondereZweckbestimmung1",
  CASE WHEN p.zweckbestimmung = 1400 THEN
     CASE WHEN p."besondereZweckbestimmung" IS NULL THEN
@@ -417,7 +417,7 @@ GRANT SELECT ON TABLE "FP_Verkehr"."FP_StrassenverkehrPunkt_qv" TO xp_gast;
 -- -----------------------------------------------------
 -- View "FP_Wasser"."FP_GewaesserFlaeche_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Wasser"."FP_GewaesserFlaeche_qv" AS 
+CREATE OR REPLACE VIEW "FP_Wasser"."FP_GewaesserFlaeche_qv" AS
  SELECT g.gid, g.position, p.zweckbestimmung as zweckbestimmung1
 FROM "FP_Wasser"."FP_GewaesserFlaeche" g
     JOIN "FP_Wasser"."FP_Gewaesser" p ON g.gid = p.gid;
@@ -426,7 +426,7 @@ GRANT SELECT ON TABLE "FP_Wasser"."FP_GewaesserFlaeche_qv" TO xp_gast;
 -- -----------------------------------------------------
 -- View "FP_Wasser"."FP_GewaesserLinie_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Wasser"."FP_GewaesserLinie_qv" AS 
+CREATE OR REPLACE VIEW "FP_Wasser"."FP_GewaesserLinie_qv" AS
  SELECT g.gid, g.position, p.zweckbestimmung as zweckbestimmung1
 FROM "FP_Wasser"."FP_GewaesserLinie" g
     JOIN "FP_Wasser"."FP_Gewaesser" p ON g.gid = p.gid;
@@ -435,7 +435,7 @@ GRANT SELECT ON TABLE "FP_Wasser"."FP_GewaesserLinie_qv" TO xp_gast;
 -- -----------------------------------------------------
 -- View "FP_Wasser"."FP_GewaesserPunkt_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Wasser"."FP_GewaesserPunkt_qv" AS 
+CREATE OR REPLACE VIEW "FP_Wasser"."FP_GewaesserPunkt_qv" AS
  SELECT g.gid, g.position, p.zweckbestimmung as zweckbestimmung1
 FROM "FP_Wasser"."FP_GewaesserPunkt" g
     JOIN "FP_Wasser"."FP_Gewaesser" p ON g.gid = p.gid;
@@ -444,7 +444,7 @@ GRANT SELECT ON TABLE "FP_Wasser"."FP_GewaesserPunkt_qv" TO xp_gast;
 -- -----------------------------------------------------
 -- View "FP_Wasser"."FP_WasserwirtschaftFlaeche_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Wasser"."FP_WasserwirtschaftFlaeche_qv" AS 
+CREATE OR REPLACE VIEW "FP_Wasser"."FP_WasserwirtschaftFlaeche_qv" AS
  SELECT g.gid, g.position, p.zweckbestimmung as zweckbestimmung1
 FROM "FP_Wasser"."FP_WasserwirtschaftFlaeche" g
     JOIN "FP_Wasser"."FP_Wasserwirtschaft" p ON g.gid = p.gid;
@@ -453,7 +453,7 @@ GRANT SELECT ON TABLE "FP_Wasser"."FP_WasserwirtschaftFlaeche_qv" TO xp_gast;
 -- -----------------------------------------------------
 -- View "FP_Wasser"."FP_WasserwirtschaftLinie_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Wasser"."FP_WasserwirtschaftLinie_qv" AS 
+CREATE OR REPLACE VIEW "FP_Wasser"."FP_WasserwirtschaftLinie_qv" AS
  SELECT g.gid, g.position, p.zweckbestimmung as zweckbestimmung1
 FROM "FP_Wasser"."FP_WasserwirtschaftLinie" g
     JOIN "FP_Wasser"."FP_Wasserwirtschaft" p ON g.gid = p.gid;
@@ -462,7 +462,7 @@ GRANT SELECT ON TABLE "FP_Wasser"."FP_WasserwirtschaftLinie_qv" TO xp_gast;
 -- -----------------------------------------------------
 -- View "FP_Wasser"."FP_WasserwirtschaftPunkt_qv"
 -- -----------------------------------------------------
-CREATE OR REPLACE VIEW "FP_Wasser"."FP_WasserwirtschaftPunkt_qv" AS 
+CREATE OR REPLACE VIEW "FP_Wasser"."FP_WasserwirtschaftPunkt_qv" AS
  SELECT g.gid, g.position, p.zweckbestimmung as zweckbestimmung1
 FROM "FP_Wasser"."FP_WasserwirtschaftPunkt" g
     JOIN "FP_Wasser"."FP_Wasserwirtschaft" p ON g.gid = p.gid;
