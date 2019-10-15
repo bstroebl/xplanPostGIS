a58888c625c6c2daaca4bfaea2c60ed79e991f79
Bernhard Ströbl <bernhard.stroebl@jena.de>
2019-10-15 11:49:31 +0200

    [BUG] Ändere Regionalplan zu Raumordnungsplan

--- createdb.sh
+++ createdb.sh
@@ -5,7 +5,7 @@ psql <neue_db> -a -f <pfad_zu_skripten>/XP_Basisschema.sql > XP_Basisschema.log 
 psql <neue_db> -a -f <pfad_zu_skripten>/BP_Fachschema_BPlan.sql > BP_Fachschema_BPlan.log 2>&1
 psql <neue_db> -a -f <pfad_zu_skripten>/FP_Fachschema_FPlan.sql > FP_Fachschema_FPlan.log 2>&1
 psql <neue_db> -a -f <pfad_zu_skripten>/LP_Fachschema_LPlan.sql > LP_Fachschema_LPlan.log 2>&1
-psql <neue_db> -a -f <pfad_zu_skripten>/RP_Fachschema_Regionalplan.sql > RP_Fachschema_Regionalplan.log 2>&1
+psql <neue_db> -a -f <pfad_zu_skripten>/RP_Fachschema_Raumordnungsplan.sql > RP_Fachschema_Raumordnungsplan.log 2>&1
 psql <neue_db> -a -f <pfad_zu_skripten>/SO_Fachschema_SonstigePlaene.sql > SO_Fachschema_SonstigePlaene.log 2>&1
 psql <neue_db> -a -f <pfad_zu_skripten>/QGIS.sql > QGIS.log 2>&1
 psql <neue_db> -a -f <pfad_zu_skripten>/XP_QGIS.sql > XP_QGIS.log 2>&1
