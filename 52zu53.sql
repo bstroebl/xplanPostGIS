9fc0c9e3e123d9f220d1bc55539e40f569643179
Bernhard Ströbl <bernhard.stroebl@jena.de>
2021-11-11 10:29:39 +0100

    [5.3] CR-003

--- 52zu53.sql
+++ 52zu53.sql
@@ -3,3 +3,7 @@
 
 -- CR-001
 INSERT INTO "RP_Freiraumstruktur"."RP_NaturLandschaftTypen" ("Code", "Bezeichner") VALUES ('2600', 'Freiraumfunktionen');
+
+-- CR-003
+INSERT INTO "XP_Basisobjekte"."XP_ExterneReferenzTyp" ("Code", "Bezeichner") VALUES (4000, 'Genehmigung');
+INSERT INTO "XP_Basisobjekte"."XP_ExterneReferenzTyp" ("Code", "Bezeichner") VALUES (5000, 'Bekanntmachung');
